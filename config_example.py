bfcb50f8af50448692498da4a4b5ea977eca1e7e
Martin Villumsen <mvil@tv2.dk>
2017-12-19 08:26:28 +0100

    Updated comment.

--- config_example.py
+++ config_example.py
@@ -1,4 +1,4 @@
-# Replace with url to Rancher and API user and pass
+# Rename to config.py and replace the following constants with the url to Rancher and API user and pass
 RANCHER_URL = "http://rancher.myurl.com/v2-beta/"
 RANCHER_API_USERNAME = "XXXXX"
 RANCHER_API_PASSWORD = "XXXXXXXXX"
